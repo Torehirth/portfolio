0dd56fad51a7bf47f2d9c60f2d4083520dfd6516
Tore Mæland Hirth <torehirth@gmail.com>
2024-10-09 20:26:04 +0200

    changed function for extended project description, so all buttons and descriptions work properly

--- src/js/ui/toggleProjectDescription.mjs
+++ src/js/ui/toggleProjectDescription.mjs
@@ -1,29 +1,84 @@
-const readMoreBtn = document.querySelector("#read-more-btn");
-const readLessBtn = document.querySelector("#read-less-btn");
-const description = document.querySelector(".description-wrapper");
-// Using the closest() method to select the related parent container (".project-wrapper")
-const scrollTarget = description.closest(".project-wrapper");
+// const descriptionWrappers = document.querySelectorAll(
+//   ".description-wrapper"
+// );
+// // Using the closest() method to select the related parent container (".project-wrapper")
+// const scrollTarget = descriptionWrappers.forEach((descriptionWrapper) => {
+//   descriptionWrapper.closest(".project-wrapper");
+// });
+
+// export const toggleProjectDescription = () => {
+//   document.addEventListener("click", (e) => {
+//     console.log(e.target);
+
+//     readMoreBtns.forEach((readMoreBtn) => {
+//       if (
+//         // if the descriptionWrappers isn't displaying, show the descriptionWrappers
+//         !descriptionWrappers.classList.contains("is-active") &&
+//         e.target === readMoreBtn
+//       ) {
+//         descriptionWrappers.classList.add("is-active");
+//         readMoreBtn.style.display = "none";
+//         // readLessBtns.style.display = "block";
+//       }
+//     });
+
+//     readLessBtns.forEach((readLessBtn) => {
+//       if (
+//         // if the descriptionWrappers is displaying, close the descriptionWrappers
+//         descriptionWrappers.classList.contains("is-active") &&
+//         e.target === readLessBtn
+//       ) {
+//         descriptionWrappers.classList.remove("is-active");
+//         readLessBtn.style.display = "none";
+//         // readMoreBtns.style.display = "block";
+//         // Scrolls to the parent container of the project when clicking "read less".
+//         scrollTarget.scrollIntoView({
+//           behavior: "smooth",
+//           block: "start",
+//         });
+//       }
+//     });
+//   });
+// };
+
+// const readMoreBtns = document.querySelectorAll(".read-more-btn");
+// const readLessBtns = document.querySelectorAll(".read-less-btn");
 
 export const toggleProjectDescription = () => {
   document.addEventListener("click", (e) => {
-    if (
-      // if the description isn't displaying, show the description
-      !description.classList.contains("is-active") &&
-      e.target === readMoreBtn
-    ) {
-      description.classList.add("is-active");
-      readMoreBtn.style.display = "none";
+    // If clicking the "Read more" button (e.target = read more button)
+    if (e.target.classList.contains("read-more-btn")) {
+      // Selecting the related/closest container with the closest() method.
+      const projectWrapper = e.target.closest(".project-wrapper");
+      const descriptionWrapper = projectWrapper.querySelector(".description-wrapper");
+      const readLessBtn = projectWrapper.querySelector(".read-less-btn");
+
+      // Display the extended description and update buttons styling
+      descriptionWrapper.classList.add("is-active");
+      e.target.style.display = "none";
       readLessBtn.style.display = "block";
-    } else if (
-      // if the description is displaying, close the description
-      description.classList.contains("is-active") &&
-      e.target === readLessBtn
-    ) {
-      description.classList.remove("is-active");
-      readLessBtn.style.display = "none";
+
+      // Scroll to the extended container when clicking read more button
+      descriptionWrapper.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+
+    // If clicking the "Read less" button (e.target = read less button)
+    if (e.target.classList.contains("read-less-btn")) {
+      // Selecting the related/closest container with the closest() method.
+      const projectWrapper = e.target.closest(".project-wrapper");
+      const descriptionWrapper = projectWrapper.querySelector(".description-wrapper");
+      const readMoreBtn = projectWrapper.querySelector(".read-more-btn");
+
+      // Hide the extended description and update buttons styling
+      descriptionWrapper.classList.remove("is-active");
+      e.target.style.display = "none";
       readMoreBtn.style.display = "block";
-      // Scrolls to the parent container of the project when clicking "read less".
-      scrollTarget.scrollIntoView({
+
+      // Scroll to the parent container of the project when clicking read less button
+      projectWrapper.scrollIntoView({
         behavior: "smooth",
         block: "start",
       });
