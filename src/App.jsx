bd88e37ba831fe8c2cb821fe8891c61cf28be30f
Tore Hirth <torehirth@gmail.com>
2025-04-08 17:16:13 +0200

    deleted scss and js folder, updated package.json and tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-export const App = () => {
+const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Welcome to React with Typescript and Tailwind CSS!
+        Welcome to my portfolio built with React, Tailwind CSS and Vite!
       </h1>
     </div>
   );
 };
+export default App;
