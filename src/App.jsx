bf4512f1e615dd09d7295a753be71c80738ec2b5
Tore Hirth <torehirth@gmail.com>
2025-04-08 19:21:48 +0200

    fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+// import Example from "@/components/Example.jsx";
+import Example from "./components/Example.jsx";
+
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Welcome to my portfolio built with React, Tailwind CSS and Vite!
-      </h1>
-    </div>
+    <>
+      <div className="flex items-center justify-center min-h-4 bg-green-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Welcome to my portfolio built with React, Tailwind CSS and Vite!
+        </h1>
+      </div>
+      <Example />
+    </>
   );
 };
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+// import typography from "@tailwindcss/typography";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: ["text-4xl", "font-bold", "text-blue-600"],
   theme: {
     extend: {},
   },
