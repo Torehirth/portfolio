4aa727c1474b06a82824cfec9c539b8c191834c4
Tore Hirth <torehirth@gmail.com>
2025-04-08 16:00:31 +0200

    add App.jsx, updated Vite config, updated version 2.0.0 in package.json

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to React with Typescript and Tailwind CSS!
+      </h1>
+    </div>
+  );
+};
