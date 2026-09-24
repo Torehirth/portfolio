90fb9e57600630b4cfe2671bca239a3511b3f5f3
Tore Hirth <torehirth@gmail.com>
2026-09-24 19:38:08 +0200

    chore: install and configure tailwind prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="bg-accent-dark">
         <h1 className="">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. A molestias
           perspiciatis sapiente molestiae veritatis, consectetur consequatur
           illo suscipit iusto repellat inventore quas quaerat, soluta facilis,
           nisi provident magni. Facere, voluptatibus.
         </h1>
-        <p className="font-mono text-h1 max-w-layout">
+        <p className="text-h1 max-w-layout border">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde deserunt
           iusto incidunt et. Odio ratione impedit et nisi quidem. Dignissimos
           dolorem veritatis harum quam aut nulla nobis ad odit. Ad?
